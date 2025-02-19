b284a86ade0666adce06b32b2a6a87ce1050d2a3
sunpra <sunpra12@gmail.com>
2025-02-19 09:00:28 +0545

    Added OptionalSubject class.

--- app/src/main/java/com/sunpra/classroom/AddStudentActivity.java
+++ app/src/main/java/com/sunpra/classroom/AddStudentActivity.java
@@ -8,13 +8,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.sunpra.classroom.databinding.ActivityAddStudentBinding;
+
 public class AddStudentActivity extends AppCompatActivity {
+
+    ActivityAddStudentBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_add_student);
+        binding = ActivityAddStudentBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
---
+++ app/src/main/java/com/sunpra/classroom/model/OptionalSubject.java
@@ -0,0 +1,5 @@
+package com.sunpra.classroom.model;
+
+public enum OptionalSubject {
+    ACCOUNTS, OPTIONAL_MATH, COMPUTER, ECONOMICS
+}
